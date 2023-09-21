a36f825888a5a15bfcf28b1778488ac3dc8c38c1
Loxo <loxo.Minecraft@gmail.com>
2023-09-21 21:08:55 +0200

    First set of blocks for the new biomes

--- src/main/java/supersymmetry/common/blocks/BlockBiomeDecoratives.java
+++ src/main/java/supersymmetry/common/blocks/BlockBiomeDecoratives.java
@@ -24,7 +24,7 @@ public class BlockBiomeDecoratives extends VariantBlock<BlockBiomeDecoratives.Bi
     public static enum BiomeDecorativeType implements IStringSerializable, IStateHarvestLevel {
 
         SULFUR_DEPOSIT("sulfur_deposit", 1),
-        ORANGE_CLAY("orange_clay", 1);
+        RED_CLAY("red_clay", 1);
 
         private final String name;
         private final int harvestLevel;
